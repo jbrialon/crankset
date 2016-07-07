d58b824582d6e0c8f712472be585fd48c1aca396
Jérém <jeremzor@gmail.com>
2016-07-07 15:48:57 +0200

    responsive + build

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,48 @@
 <template lang="jade">
-.container
+.container#app
   .row
-    .col.s6.offset-s3
-      #app
-        .row
-          .col.s12.align-center
-            h1(v-text="'Crankset'")
-        .row
-          .input-field.col.s6
-            input(type="text", placeholder="plateau", v-model="plateau", id="plateau")
-            label(for="plateau" v-text="'Plateau (dents)'", class="active")
-          .input-field.col.s6
-            input(type="text", placeholder="pignon", v-model="pignon", id="pignon")
-            label(for="pignon" v-text="'Pignon (dents)'", class="active")
-        .row
-          h4(v-text="' Ratio : '")
-          span
-            strong(v-html="ratio")
-        .row
-          .input-field.col.s6
-            input(type="text", placeholder="vitesse", v-model="speed", id="vitesse")
-            label(for="vitesse" v-text="'Vitesse en km/h'", class="active")
-          .input-field.col.s6
-            input(type="text", placeholder="circonference", v-model="circonference", id="circonference")
-            label(for="circonference" v-text="'Circonference de la roue en metres'", class="active")
-        .row
-          h4(v-text="'Cadence : '")
-          span
-            strong(v-html="cadence")
-            span(v-text="' tr/min'")
-            a.btn-floating.btn-large.red.right(@click="add()")
-              i.material-icons add
-        .row
-          table.striped
+    .col.m6.offset-m3.s12
+      .row
+        .col.s12.align-center.center-align
+          h1(v-text="'Crankset'")
+      .row
+        .input-field.col.m6.s12
+          input(type="text", placeholder="plateau", v-model="plateau", id="plateau")
+          label(for="plateau" v-text="'Plateau (dents)'", class="active")
+        .input-field.col.m6.s12
+          input(type="text", placeholder="pignon", v-model="pignon", id="pignon")
+          label(for="pignon" v-text="'Pignon (dents)'", class="active")
+      .row
+        h4(v-text="' Ratio : '")
+        p
+          strong(v-html="ratio")
+      .row
+        .input-field.col.m6.s12
+          input(type="text", placeholder="vitesse", v-model="speed", id="vitesse")
+          label(for="vitesse" v-text="'Vitesse en km/h'", class="active")
+        .input-field.col.m6.s12
+          input(type="text", placeholder="circonference", v-model="circonference", id="circonference")
+          label(for="circonference" v-text="'Circonference de la roue en metres'", class="active")
+      .row
+        h4(v-text="'Cadence : '")
+        p
+          strong(v-html="cadence")
+          span(v-text="' tr/min'")
+          a.btn-floating.btn-large.red.right(@click="add()")
+            i.material-icons add
+      .row(v-if="dataset.length > 0")
+        .col.s12
+          table.striped.responsive-table
             thead
               tr
                 th(data-field='id') Plateau
-                th(data-field='name') pignon
-                th(data-field='ratio') ratio
-                th(data-field='speed') speed
-                th(data-field='circonference') circonference
-                th(data-field='cadence') cadence
-                th(data-field='gain') gain
-                th(data-field='action') action
+                th(data-field='name') Pignon
+                th(data-field='ratio') Ratio
+                th(data-field='vitesse') Vitesse
+                th(data-field='circonference') Circonference
+                th(data-field='cadence') Cadence
+                th(data-field='gain') Gain
+                th(data-field='action') Action
             tbody
               tr(v-for="data in dataset" track-by="$index")
                 td(v-text="data.plateau")
